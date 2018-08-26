909ef72a42d04690b431f60398ccb50acc08f54a
sthigabout <hkdt.10@gmail.com>
2018-08-26 06:51:56 +0000

    ajaxでPOST

--- app.js
+++ app.js
@@ -14,36 +14,18 @@ const Tweet = mongoose.model("Tweet",tweetSchema)
 app.set("view engine", "ejs")
 
 app.use(express.static(__dirname + "/assets"))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.get("/", function(req,res){
-  const promise = Tweet.find({})
-  promise.then(function(tweets){
-    console.log(tweets)
-  })
-  Tweet.find({}, function(err,tweets){
-    if(err){
-      console.log(err)
-    } else{
-      res.render("app",{tweets:tweets})
-    }
-  })
-  // res.render("app", {tweet: ""})
-
+app.get("/", async function(req,res){
+  const tweets  = await Tweet.find({})
+  const reversedTweets = tweets.reverse()
+  res.render("app",{tweets})
 })
 
-app.post("/tweet", function(req,res){
+app.post("/", function(req,res){
 
-  console.log(req.body)
   Tweet.create(req.body)
-  Tweet.find({}, function(err,tweets){
-    if(err){
-      console.log(err)
-    } else{
-      res.render("app",{tweets:tweets})
-      console.log(tweets)
-    }
-  })   
+   res.send(req.body)
 })
 
 app.listen(8000, function(){
