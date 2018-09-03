ce1f2adbb1371761d0b89d05cf1a664a181a8b54
sthigabout <hkdt.10@gmail.com>
2018-09-03 18:33:11 +0000

    passport-local実装

--- app.js
+++ app.js
@@ -2,31 +2,36 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 const mongoose = require("mongoose")
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const passportLocalMongoose = require("passport-local-mongoose")
+const User = require("./models/user")
 
 mongoose.connect("mongodb://localhost:27017/untitled_sns", { useNewUrlParser: true })
 
-const tweetSchema = new mongoose.Schema({
-  content:String
-})
-
-const Tweet = mongoose.model("Tweet",tweetSchema)
-
 app.set("view engine", "ejs")
-
 app.use(express.static(__dirname + "/assets"))
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(require("express-session")({
+  secret: "The cake is lie",
+  resave: false,
+  saveUninitialized: false
+}));
 
-app.get("/", async function(req,res){
-  const tweets  = await Tweet.find({})
-  const reversedTweets = tweets.reverse()
-  res.render("app",{tweets})
-})
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post("/", function(req,res){
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-  Tweet.create(req.body)
-   res.send(req.body)
-})
+const tweetRoutes    = require("./routes/tweets")
+const indexRoutes    = require("./routes/index")
+
+app.use("/", tweetRoutes)
+app.use("/",indexRoutes)
+app.use("/register",indexRoutes)
+app.use("/login",indexRoutes)
 
 app.listen(8000, function(){
   console.log("server started")
