828fcd9c72fc58cc457dfc76cdcec9b1fa00d2e1
sthigabout <hkdt.10@gmail.com>
2018-12-22 19:37:19 +0900

    test2

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hellllllo');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
 app.listen(8080 || process.env.PORT, function() {
-  console.log('server started');
+  res.render;
 });
