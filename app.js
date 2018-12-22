47129fef288a644b85ebdcdbd8cac243cd4649af
sthigabout <hkdt.10@gmail.com>
2018-12-22 19:32:52 +0900

    test

--- app.js
+++ app.js
@@ -1,57 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const passportLocalMongoose = require('passport-local-mongoose');
-const User = require('./models/user');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const _ = require('lodash');
 
-// const redis   = require("redis")
-// const redisStore = require('connect-redis')(session)
-// const client  = redis.createClient()
-
-mongoose.connect(
-  MONGODB_URI,
-  { useNewUrlParser: true }
-);
-
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/assets'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 100000,
-      autoRemove: 'native',
-      collection: 'AllSessions'
-    }),
-    secret: 'The cake is lie',
-    // store: new redisStore({ host: 'localhost', port: 8000, client: client,ttl :  10}),
-    resave: false,
-    saveUninitialized: false
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-const tweetRoutes = require('./routes/tweets');
-const indexRoutes = require('./routes/index');
-const userRoutes = require('./routes/user');
-
-app.use('/', tweetRoutes);
-app.use('/', indexRoutes);
-app.use('/user', userRoutes);
+app.get('/', (req, res) => {
+  res.send('hellllllo');
+});
 
 app.listen(8080 || process.env.PORT, function() {
   console.log('server started');
